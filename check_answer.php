1237acd1ea71ad3a8420f04068eb3d15b2215a6e
Kris Jane Cabilan <cabilankrisjane@gmail.com>
2024-01-13 21:40:26 +0800

    add questions

--- check_answer.php
+++ check_answer.php
@@ -5,20 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
     <title>Result</title>
 </head>
 
 <body>
 
 <!-- Modal -->
-<div class="modal" tabindex="-1" id="resultModal">
+<div class="modal animated fadeIn" tabindex="-1" id="resultModal">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <h2 class="modal-title">Result</h2>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
             </div>
             <div class="modal-body">
                 <?php
@@ -34,7 +32,20 @@
                     "Which planet is known as the Red Planet?" => "Mars",
                     "What is the largest mammal in the world?" => "Blue Whale",
                     "What is the capital of Philippines?" => "Manila",
-                    // Add more questions as needed
+                    "What is the process of finding and fixing errors in a program?" => "Debug",
+                    " What type of loop repeats a block of code while a specified condition is true?" => "While",
+                    "In programming, what is a collection of related data items called?" => "Array",
+                    "What is the result of 2 + 2 in most programming languages?" => "4",
+                    "What is the symbol of gold in periodic table?" => "Au",
+                    "What is the biggest star?" => "Sun",
+                    "What is the largest planet in our solar system?" => "Jupiter",
+                    "What is the primary purpose of a 'for' loop in programming?" => "Iteration",
+                    "In web development, what does CSS stand for?" => "Cascading",
+                    " What data type is used for whole numbers in many programming languages?" => "Integer",
+                    "Which programming language is often used for server-side web development?" => "Node.js",
+                    "What is the default file extension for a Python script?" => "py",
+                    "What is the result of dividing any number by zero in mathematics?" => "Undefined",
+                    "In web development, what language is used to style the presentation of a webpage?" => "CSS",
                 );
 
                 // Get the correct answer for the displayed question
