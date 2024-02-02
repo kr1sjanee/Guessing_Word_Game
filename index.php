1baf8413c169961299e632b4629cbb5361e7de31
John Louie Navales <104504427+johnlouie09@users.noreply.github.com>
2024-02-02 22:25:32 +0800

    feat: add new question to test this commit

--- index.php
+++ index.php
@@ -40,7 +40,8 @@
         "What is the name of the version control system widely used in software development?" => "Git",
         "Sino ang pumatay kay Lapu-Lapu" => "Magellan",
         "What is the framework created by Evan You?" => "Vue",
-        "Pogi ba ako?" => "Oo"
+        "Pogi ba ako?" => "Oo",
+        "Ano sa Tagalog ng Test?" => "Test"
     );
 
     // Display a random question when the page loads
